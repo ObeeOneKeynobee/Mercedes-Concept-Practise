17ec41541e7c12a0d42ac2844ceb9ae139e41407
Denis Skalin <den.skalin@gmail.com>
2022-08-06 19:13:44 +0300

    layout completed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-$(function(){
-
+$(function () {
+  $(".desighn__slider").slick({
+    dots: false,
+    slidesToShow: 4,
+    variableWidth: true,
+    prevArrow:
+      '<img src="/img/ArrowLeft.svg" alt="" class="arrow arrow-left" />',
+    nextArrow:
+      '<img src="/img/ArrowRight.svg" alt="" class="arrow arrow-right" />',
+  });
 });
